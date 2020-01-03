217e8a8000b7175f2da0e143ee509fc0eb837f23
Egy Ramadhan <36892796+egyramadhan@users.noreply.github.com>
2020-01-03 15:03:29 +0700

    notifications handler

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -92,8 +92,62 @@ class PaymentController extends Controller
         // return $paymentUrl;
     }
 
-    public function testing(){
+    public function notifications(){
+        $notif = new \Midtrans\Notification();
+        $transaction = $notif->transaction_status;
+        $fraud = $notif->fraud_status;
 
+        error_log("Order ID $notif->order_id: "."transaction status = $transaction, fraud staus = $fraud");
+
+
+        if ($transaction == 'capture') {
+            // For credit card transaction, we need to check whether transaction is challenge by FDS or not
+            if ($type == 'credit_card') {
+                if ($fraud == 'challenge') {
+                    // TODO set payment status in merchant's database to 'Challenge by FDS'
+                    // TODO merchant should decide whether this transaction is authorized or not in MAP
+                    echo "Transaction order_id: " . $order_id ." is challenged by FDS";
+                } else {
+                    // TODO set payment status in merchant's database to 'Success'
+                    echo "Transaction order_id: " . $order_id ." successfully captured using " . $type;
+                }
+            }
+        } else if ($transaction == 'settlement') {
+            // TODO set payment status in merchant's database to 'Settlement'
+            echo "Transaction order_id: " . $order_id ." successfully transfered using " . $type;
+        } else if ($transaction == 'pending') {
+            // TODO set payment status in merchant's database to 'Pending'
+            echo "Waiting customer to finish transaction order_id: " . $order_id . " using " . $type;
+        } else if ($transaction == 'deny') {
+            // TODO set payment status in merchant's database to 'Denied'
+            echo "Payment using " . $type . " for transaction order_id: " . $order_id . " is denied.";
+        } else if ($transaction == 'expire') {
+            // TODO set payment status in merchant's database to 'expire'
+            echo "Payment using " . $type . " for transaction order_id: " . $order_id . " is expired.";
+        } else if ($transaction == 'cancel') {
+            // TODO set payment status in merchant's database to 'Denied'
+            echo "Payment using " . $type . " for transaction order_id: " . $order_id . " is canceled.";
+        }
+
+        // if ($transaction == 'capture') {
+        //     if ($fraud == 'challenge') {
+        //     // TODO Set payment status in merchant's database to 'challenge'
+        //     }
+        //     else if ($fraud == 'accept') {
+        //     // TODO Set payment status in merchant's database to 'success'
+        //     }
+        // }
+        // else if ($transaction == 'cancel') {
+        //     if ($fraud == 'challenge') {
+        //     // TODO Set payment status in merchant's database to 'failure'
+        //     }
+        //     else if ($fraud == 'accept') {
+        //     // TODO Set payment status in merchant's database to 'failure'
+        //     }
+        // }
+        // else if ($transaction == 'deny') {
+        //     // TODO Set payment status in merchant's database to 'failure'
+        // }
     }
 }
 
